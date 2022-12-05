c33e41f69b7a4230a3491fe4f9d288a166a1e750
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-05 12:16:07 +0700

    add note, add h3 to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,9 @@ export default function Navbar() {
     <div className="w-full h-16 fixed top-0 z-30">
       <nav className="h-full bg-black bg-opacity-80 ">
         <div className="h-1/2 py-3">
-          <div className="container flex flex-row justify-center pt-10"></div>
+          <div className="container flex flex-row justify-center pt-10">
+            <h3></h3>
+          </div>
         </div>
       </nav>
     </div>
