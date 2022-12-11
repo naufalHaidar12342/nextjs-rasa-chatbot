6c2a2a0c0a683c6b83d672ed058fb61cdd07ab0a
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-11 19:55:48 +0700

    add framer-motion package, move blur layer to Layout, change button size for help and report

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,10 +9,16 @@ export default function Layout({ children, title = "" }) {
 				<title>{title}</title>
 			</Head>
 			<main className="relative px-[156px] py-[84px] h-screen">
-				<BlurLayer>
+				<div
+					className="relative overflow-hidden z-20 p-20 rounded-[32px] flex items-center flex-col"
+					style={{
+						backdropFilter: "saturate(100%) blur(20px)",
+						background: "rgba(255, 255, 255, 0.25)",
+					}}
+				>
 					{children}
 					<Footer />
-				</BlurLayer>
+				</div>
 
 				<Image
 					src="/images/background.svg"
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,50 +1,56 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../components/Layout";
 import { MdQuestionAnswer } from "react-icons/md";
 import { MdReportProblem } from "react-icons/md";
+import { motion } from "framer-motion";
 export default function Home() {
 	return (
 		<Layout title="Home">
-			<div className="text-[50px] font-medium">Learning Chatbot</div>
-			<div className="text-[90px] font-bold">SMA LOYOLA</div>
-			<div
-				className="flex justify-center items-center space-x-11 space-y-3"
-				title="Button organizer"
+			<motion.div
+				className="flex flex-col items-center"
+				animate={{ opacity: 1, scale: 1 }}
+				initial={{ opacity: 0, scale: 0.5 }}
+				transition={{ duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01] }}
 			>
+				<div className="text-[50px] font-medium">Learning Chatbot</div>
+				<div className="text-[90px] font-bold">SMA LOYOLA</div>
 				<div
-					title="Help button"
-					className="rounded-full w-40 h-40 flex flex-col justify-center items-center"
-					style={{
-						backdropFilter: "saturate(100%) blur(20px)",
-						background: "rgba(255, 255, 255, 0.25)",
-					}}
+					className="flex justify-center items-center space-x-11 space-y-3"
+					title="Button organizer"
 				>
-					<MdQuestionAnswer className="text-7xl" />
-					<div className="text-3xl">Help</div>
+					<div
+						title="Help button"
+						className="rounded-full w-48 h-48 flex flex-col justify-center items-center"
+						style={{
+							backdropFilter: "saturate(100%) blur(20px)",
+							background: "rgba(255, 255, 255, 0.25)",
+						}}
+					>
+						<MdQuestionAnswer className="text-7xl" />
+						<div className="text-3xl">Help</div>
+					</div>
+					<div
+						title="Start button"
+						className="rounded-full w-[300px] h-[300px] flex flex-col justify-center items-center"
+						style={{
+							backdropFilter: "saturate(100%) blur(20px)",
+							background: "rgba(255, 255, 255, 0.25)",
+						}}
+					>
+						<div className="text-7xl">Start</div>
+					</div>
+					<div
+						title="Report problem button"
+						className="rounded-full w-48 h-48 flex flex-col justify-center items-center"
+						style={{
+							backdropFilter: "saturate(100%) blur(20px)",
+							background: "rgba(255, 255, 255, 0.25)",
+						}}
+					>
+						<MdReportProblem className="text-7xl" />
+						<div className="text-3xl">Report </div>
+					</div>
 				</div>
-				<div
-					title="Start button"
-					className="rounded-full w-[300px] h-[300px] flex flex-col justify-center items-center"
-					style={{
-						backdropFilter: "saturate(100%) blur(20px)",
-						background: "rgba(255, 255, 255, 0.25)",
-					}}
-				>
-					<div className="text-7xl">Start</div>
-				</div>
-				<div
-					title="Report problem button"
-					className="rounded-full w-40 h-40 flex flex-col justify-center items-center"
-					style={{
-						backdropFilter: "saturate(100%) blur(20px)",
-						background: "rgba(255, 255, 255, 0.25)",
-					}}
-				>
-					<MdReportProblem className="text-7xl" />
-					<div className="text-3xl">Report </div>
-				</div>
-			</div>
+			</motion.div>
 		</Layout>
 	);
 }
