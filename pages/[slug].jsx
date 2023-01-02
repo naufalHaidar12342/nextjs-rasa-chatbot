b357c3d706de86b519db4c8e5b002c2194a5b0b2
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-01-02 23:51:25 +0700

    fixed bug where tailwind color won't shows up as recommendation, adding text input and container for microphone

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import { useEffect, useState } from "react";
-
+import { MdMic } from "react-icons/md";
 const sendData = (ws, data) => {
 	ws.send(
 		JSON.stringify({
@@ -9,9 +9,139 @@ const sendData = (ws, data) => {
 	);
 };
 
+const data = [
+	{
+		created_at: 1,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 2,
+		user_id: 2,
+		name: "Doe",
+		text: "world",
+	},
+	{
+		created_at: 3,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 4,
+		user_id: 1,
+		name: "John",
+		text: "hello",
+	},
+	{
+		created_at: 5,
+		user_id: 1,
+		name: "John",
+		text: "ini baru",
+	},
+];
+
 export default function Chat() {
 	const ENDPOINT = "ws://";
 	const [ws, setWs] = useState();
+	const [messages, setMessages] = useState(data);
 
 	// // koneksi websocket
 	// useEffect(() => {
@@ -31,9 +161,91 @@ export default function Chat() {
 	// 	});
 	// }, []);
 
+	// useEffect(() => {
+	// 	if (ws) {
+	// 		ws.onmessage = (message) => {
+	// 			const messageData = JSON.parse(message.data);
+
+	// 			if (messageData.type === "new-message") {
+	// 				setMessages([...messages, messageData.message]);
+	// 			}
+	// 		};
+	// 	}
+	// });
+
+	const mydId = 1;
+
+	const handleSubmit = () => {};
+
+	function compare(a, b) {
+		if (a.created_at > b.created_at) {
+			return -1;
+		}
+		if (a.created_at > b.created_at) {
+			return 1;
+		}
+		return 0;
+	}
+
 	return (
 		<Layout title="Chat Screen">
-			<div className=""></div>
+			<style jsx global>{`
+				/* width */
+				.scroll::-webkit-scrollbar {
+					width: 20px;
+				}
+
+				/* Track */
+				.scroll::-webkit-scrollbar-track {
+					box-shadow: inset 0 0 5px grey;
+					border-radius: 10px;
+				}
+
+				.scroll::-webkit-scrollbar-thumb {
+					background: black;
+					border-radius: 16px;
+				}
+			`}</style>
+			<div className=" bg-white p-5 w-full text-black rounded-2xl">
+				<div className="scroll flex flex-col-reverse overflow-y-scroll h-[400px]">
+					{data.sort(compare).map((item) => {
+						return (
+							<div
+								key={item.user_id}
+								className={`rounded-2xl py-1 px-5 mb-1 ${
+									item.user_id === 1
+										? "bg-blue-300 self-start"
+										: "bg-red-500 self-end"
+								}`}
+							>
+								<div>{item.name}</div>
+								<div>{item.text}</div>
+							</div>
+						);
+					})}
+				</div>
+				<div
+					id="input"
+					className="rounded-2xl bg-darkerYellow p-5 bottom-0 mt-5 h-24 flex flex-row space-x-4 items-center"
+				>
+					<form onSubmit={handleSubmit} className="h-full w-full">
+						<input
+							type="text"
+							name=""
+							id=""
+							className="w-full h-full rounded-2xl text-2xl p-5 bg-white focus:outline-none"
+						/>
+					</form>
+					<form onSubmit={handleSubmit} className="h-full w-24">
+						<input
+							type="text"
+							name=""
+							id=""
+							className="w-full h-full rounded-full text-2xl p-5 bg-white focus:outline-none"
+						/>
+					</form>
+				</div>
+			</div>
 		</Layout>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
 	content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
 	theme: {
-		extend: {},
-		colors: {
-			primary: "#003399",
-			secondary2: "#D3D3D3",
-			darkerYellow: "#C8940F",
-			white: "#FFFFFF",
+		extend: {
+			colors: {
+				primary: "#003399",
+				secondary2: "#D3D3D3",
+				darkerYellow: "#C8940F",
+				white: "#FFFFFF",
+			},
 		},
 	},
 	plugins: [require("tailwindcss"), require("autoprefixer")],
