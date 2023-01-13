631e7c00d2a6d6b30eb1c58f57ebc609c2abaaff
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-01-13 19:24:07 +0700

    added one of the font to _app.js, bug regarding blacked font still persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "../styles/globals.css";
+import { Poppins, Space_Grotesk } from "@next/font/google";
+const poppins = Poppins({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	style: ["italic", "normal"],
+	display: "swap",
+	subsets: ["devanagari", "latin", "latin-ext"],
+});
 
+const spaceGrotesk = Space_Grotesk({
+	weight: ["300"],
+	style: ["normal"],
+	display: "swap",
+	subsets: ["latin", "latin-ext", "vietnamese"],
+});
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={poppins.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
 export default MyApp;
