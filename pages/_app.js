b3e171f25f30f2391fa59590e2fe33095495cfac
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-01-15 00:42:31 +0700

    replace font being used, add font through next/font, replace modal template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
-
+import { Poppins } from "@next/font/google";
+const poppins = Poppins({
+	subsets: ["latin", "devanagari", "latin-ext"],
+	weight: ["400", "500", "600", "700"],
+});
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={poppins.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
 export default MyApp;
