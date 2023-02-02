fc04905c2c0d49bf29dbc2b4cfe9e3944a477f5a
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-02-02 23:31:04 +0700

    add dotenv dependency, currently still finding the best config for sendgrid API

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 		await sendgrid.send({
 			to: "chatbotpendampingguru@gmail.com",
 			from: "chatbotpendampingguru@gmail.com",
-			subject: `${req.body.subject}`,
+			subject: `Laporan Masalah Chatbot`,
 			html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html
 	data-editor-version="2"
